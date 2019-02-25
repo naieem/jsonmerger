b210759b84151a48f5d574c996444abeb3355b1a
Naieem Mahmud Supto <naieemsupto@gmail.com>
2019-02-25 16:51:41 +0600

    updated the matching thing

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    file1: {
+        name: 'client file',
+        path: './files/client_de_new.json'
+    },
+    file2: {
+        name: 'local file',
+        path: './files/prod_de_old.json'
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var lodash = require('lodash');
 var router = express.Router();
 var fs = require('fs');
+var config = require('../config');
 router.get('/', function(req, res, next) {
-    res.status(200).json({
-        message: 'yah it works'
-    });
+    res
+        .status(200)
+        .json({ message: 'yah it works' });
 });
 var newFilesData; // saves new files data
 var oldFilesdata; // saves old files data
@@ -14,122 +15,110 @@ var oldFilesKey = []; // holds old files all key
 /**
  * read new files data
  */
-fs.readFile('./files/new.json', 'utf8', function(err, data) {
+fs.readFile(config.file1.path, 'utf8', function(err, data) {
     newFilesData = JSON.parse(data);
-    lodash.forEach(newFilesData, function(value, key) {
-        newFilesKey.push(key);
-    });
-    console.log('new file read done');
+    newFilesKey = Object.keys(newFilesData);
+    console.log(config.file1.name + ' read done');
+    console.log('Total Key found ' + newFilesKey.length + ' in ' + config.file1.name);
 });
 
 /**
  * read old files data
  */
-fs.readFile('./files/old.json', 'utf8', function(err, oldData) {
+fs.readFile(config.file2.path, 'utf8', function(err, oldData) {
     oldFilesdata = JSON.parse(oldData);
-    lodash.forEach(oldFilesdata, function(value, key) {
-        oldFilesKey.push(key);
-    });
-    console.log('Old file read done');
+    oldFilesKey = Object.keys(oldFilesdata);
+    console.log(config.file1.name + ' read done');
+    console.log('Total Key found ' + oldFilesKey.length + ' in ' + config.file2.name);
 });
 
-
 router.get('/getExistingchanges', function(req, res, next) {
-
-    getExistingchanges().then(function(result) {
-        res.status(200).json({
-            LatestChanges: {
-                length: result.length,
-                data: result
-            },
-            // olddatainfo: {
-            //     length: oldFilesKey.length,
-            //     data: oldFilesKey
-            // },
-            // newdata: {
-            //     length: newFilesKey.length,
-            //     data: newFilesKey
-            // }
+    getExistingchanges()
+        .then(function(result) {
+            res
+                .status(200)
+                .json({
+                    LatestChanges: {
+                        length: result.length,
+                        data: result
+                    }
+                });
         });
-    });
 
 });
 
 router.get('/getnewaddedchanges', function(req, res, next) {
-    getnewaddedchanges().then(function(result) {
-        res.status(200).json({
-            newaddedchanges: {
-                length: result.length,
-                data: result
-            },
-            // olddatainfo: {
-            //     length: oldFilesKey.length,
-            //     data: oldFilesKey
-            // },
-            // newdata: {
-            //     length: newFilesKey.length,
-            //     data: newFilesKey
-            // }
+    getnewaddedchanges()
+        .then(function(result) {
+            res
+                .status(200)
+                .json({
+                    newaddedchanges: {
+                        length: result.length,
+                        data: result
+                    }
+                });
         });
-    });
 });
 
 /**
  * return the merged files endpoint
  */
 router.get('/getmergedFile', function(req, res, next) {
-    getmergedFile().then(function(result) {
-        // get newly added keys and append them to the old key
-        if (result) {
-            lodash.forEach(result, function(value, key) {
-                oldFilesdata[key] = value;
+    getmergedFile()
+        .then(function(result) {
+            var dataTobewritten = JSON.stringify(result); // stringify the object to save in the file
+            fs.writeFile('result/result.json', dataTobewritten, function(err) {
+                if (err) {
+                    res
+                        .status(200)
+                        .json({ message: err });
+                } else {
+                    console.log('new merged file created!');
+                    res
+                        .status(200)
+                        .json({ message: "new merged file created!", data: result });
+                }
             });
-        }
-        var dataTobewritten = JSON.stringify(oldFilesdata); // stringify the object to save in the file
-        fs.writeFile('result/result.json', dataTobewritten, function(err) {
-            if (err) {
-                res.status(200).json({
-                    message: err
-                });
-            } else {
-                console.log('new merged file created!');
-                res.status(200).json({
-                    message: "new merged file created!",
-                    data: oldFilesdata
-                });
-            }
         });
-    });
 });
 
 /**
  * getting the merged file ready for future use
  */
 function getmergedFile() {
-    return new Promise(function(resolve, reject) {
-        getExistingchanges().then(function(result) {
-            // if there are exising data that changed
-            if (result && result.length) {
-                lodash.forEach(result, function(value) {
-                    lodash.forEach(value.changes, function(langVal) {
-                        oldFilesdata[value.key][langVal.lang] = langVal.newValue;
-                    });
-                });
-                console.log('Existing value changes done');
-            } else {
-                console.log('No existing value changes found');
-            }
-            // return newly added keys
-            getnewaddedchanges().then(function(result) {
-                if (result && result.length) {
-                    var newValues = {};
-                    lodash.forEach(result, function(value) {
-                        newValues[value.key] = value.value;
-                    });
+    return new Promise((resolve, reject) => {
+        let updatedData = null;
+        getExistingchanges()
+            .then((result) => {
+                if (oldFilesKey.length > newFilesKey.length) {
+                    updatedData = oldFilesdata;
                 }
-                resolve(newValues);
+                if (oldFilesKey.length < newFilesKey.length) {
+                    updatedData = newFilesData;
+                }
+                // if there are exising data that changed
+                if (result && result.length) {
+                    result.forEach((latestData, index) => {
+                        if (updatedData[latestData.key]) {
+                            updatedData[latestData.key] = latestData.newValue;
+                        }
+                    });
+                    console.log('Existing value changes done');
+                } else {
+                    console.log('No existing value changes found');
+                }
+                // return newly added keys
+                getnewaddedchanges()
+                    .then((result) => {
+                        if (result && result.length) {
+                            result.forEach((res, index) => {
+                                updatedData[res.key] = res.value;
+                            });
+                        }
+                        resolve(updatedData);
+                    });
             });
-        });
     });
 }
 
@@ -141,42 +130,11 @@ function getmergedFile() {
 function getExistingchanges() {
     var changedValue = [];
     return new Promise(function(resolve, reject) {
-        lodash.forEach(newFilesData, function(value, key) {
-            if (lodash.indexOf(oldFilesKey, key) > -1) {
-                var langchanges = [];
-                if (oldFilesdata[key]['en'] != value['en']) {
-                    langchanges.push({
-                        lang: 'en',
-                        oldValue: oldFilesdata[key]['en'],
-                        newValue: value['en']
-                    });
+        newFilesKey.forEach((key, index) => {
+            if (oldFilesKey.includes(key)) {
+                if (newFilesData[key] != oldFilesdata[key]) {
+                    changedValue.push({ key: key, oldValue: oldFilesdata[key], newValue: newFilesData[key] });
                 }
-                if (oldFilesdata[key]['cz'] != value['cz']) {
-                    langchanges.push({
-                        lang: 'cz',
-                        oldValue: oldFilesdata[key]['cz'],
-                        newValue: value['cz']
-                    });
-                }
-                if (oldFilesdata[key]['de'] != value['de']) {
-                    langchanges.push({
-                        lang: 'de',
-                        oldValue: oldFilesdata[key]['de'],
-                        newValue: value['de']
-                    });
-                }
-                if (langchanges.length) {
-                    var obj = {
-                        key: key,
-                        changes: langchanges.length ? langchanges : 'No changes found'
-                    }
-                    changedValue.push(obj)
-                }
-                // var obj = {
-                //     key: key,
-                //     language: langchanges.length ? langchanges : 'No changes found'
-                // }
-                // changedValue.push(obj)
             }
         });
         resolve(changedValue);
@@ -190,14 +148,9 @@ function getExistingchanges() {
 function getnewaddedchanges() {
     return new Promise(function(resolve, reject) {
         var changedValue = [];
-        lodash.forEach(newFilesData, function(value, key) {
-            if (lodash.indexOf(oldFilesKey, key) == -1) {
-                var langchanges = [];
-                var obj = {
-                    key: key,
-                    value: value
-                }
-                changedValue.push(obj);
+        newFilesKey.forEach((key, index) => {
+            if (!oldFilesKey.includes(key)) {
+                changedValue.push({ key: key, value: newFilesData[key] });
             }
         });
         resolve(changedValue);
